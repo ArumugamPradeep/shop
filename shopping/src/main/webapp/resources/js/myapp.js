83b7c03cc473b5f7afdafee8a870727d8d250e8a
Pradeep <pradeepugamathi@gmail.com>
2017-08-23 15:10:59 +0530

    Fixed jquery table

--- shopping/src/main/webapp/resources/js/myapp.js
+++ shopping/src/main/webapp/resources/js/myapp.js
@@ -33,58 +33,76 @@ $(function() {
 		if (window.categoryId == '') {
 			jsonUrl = window.contextRoot + '/json/data/all/products';
 		} else {
-			jsonUrl = window.contextRoot + '/json/data/category/'+ window.categoryId +'/products';
+			jsonUrl = window.contextRoot + '/json/data/category/'
+					+ window.categoryId + '/products';
 
 		}
 
-		$table.DataTable({
-			lengthMenu : [ [ 3, 5, 10, -1 ],
-					[ '3 Records', '5 Records', '10 Records', 'ALL' ] ],
-			pageLength : 5,
+		$table
+				.DataTable({
+					lengthMenu : [ [ 3, 5, 10, -1 ],
+							[ '3 Records', '5 Records', '10 Records', 'ALL' ] ],
+					pageLength : 5,
 
-			ajax : {
-				url : jsonUrl,
-				dataSrc : ''
-			},
+					ajax : {
+						url : jsonUrl,
+						dataSrc : ''
+					},
 
-			columns : [	
-				
-				
-				
-				{
-				data : 'code'
-			},
+					columns : [
 
-			{
-				data : 'brand'
-			},
+							{
+								data : 'code'
+							},
 
-			{
-				data : 'unitPrice',
-				mRender: function(data, type, row){
-					return '&#8377; ' + data
-				}
-			},
+							{
+								data : 'brand'
+							},
 
-			{
-				data : 'quantity'
-			},
-			{
-				data: 'id',
-				bSortable: false,
-                mRender: function(data, type, row){
-						
-						var str = '';
-						str += '<a href="'+window.contextRoot+ '/show/'+data+'/product"class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';
-						
-						str += '<a href="'+window.contextRoot+ '/cart/add/'+data+'/product"class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
-						return str;
-                }
-			}
-			
-			]
+							{
+								data : 'unitPrice',
+								mRender : function(data, type, row) {
+									return '&#8377; ' + data
+								}
+							},
 
-		});
+							{
+								data : 'quantity',
+								mRender : function(data, type, row) {
+
+									if (data < 1) {
+										return '<span style="color:red"> Out Of Stock </span>';
+
+									}
+									return data;
+								}
+							},
+							{
+								data : 'id',
+								bSortable : false,
+								mRender : function(data, type, row) {
+
+									var str = '';
+									str += '<a href="'	+ window.contextRoot + '/show/'	+ data
+											+ '/product"class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a> &#160;';
+
+									if(row.quantity < 1){
+										str += '<a href="javascript:void(0)" class="btn btn-success disabled"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+	 
+									}
+									else{
+										
+										str += '<a href="'	+ window.contextRoot	+ '/cart/add/'	+ data
+										+ '/product"class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
+									
+									
+									}return str;
+								}
+							}
+
+					]
+
+				});
 
 	}
 
