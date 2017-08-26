fbf13b4661587e9c5587b46007e5a27491b76588
Pradeep <pradeepugamathi@gmail.com>
2017-08-26 08:33:02 +0530

    6-7 done

--- shopping/src/main/java/deep/shopping/controller/PageController.java
+++ shopping/src/main/java/deep/shopping/controller/PageController.java
@@ -18,130 +18,117 @@ import deep.shoppingbackend.dto.Product;
 public class PageController {
 
 	public static final Logger logger = LoggerFactory.getLogger(PageController.class);
-	
-	
+
 	@Autowired
 	private CategoryDAO categoryDAO;
-	
+
 	@Autowired
 	private ProductDAO productDAO;
 
-	
-	@RequestMapping(value= {"/","home","/index"})
-	public ModelAndView index()
-	{
-		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("title","Home");
-		
+	@RequestMapping(value = { "/", "home", "/index" })
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+
 		logger.info("Inside PageController index method - INFO");
 		logger.debug("Inside PageController index method- DEBUG");
-	
-		
-		//passing list of category
+
+		// passing list of category
 		mv.addObject("categories", categoryDAO.list());
-		
-		mv.addObject("userClickHome",true);
-        return mv;
-		
-}
-	
-	
-	@RequestMapping(value="/about")
-	public ModelAndView about()
-	{
-		ModelAndView mv=new ModelAndView("page");
-		
-		mv.addObject("title","About Us");
-		mv.addObject("userClickAbout",true);
-		
+
+		mv.addObject("userClickHome", true);
 		return mv;
-		
+
 	}
 
-	@RequestMapping(value="/contact")
-	public ModelAndView contact()
-	{
-		ModelAndView mv=new ModelAndView("page");
-		
-		mv.addObject("title","Contact Us");
-		mv.addObject("userClickContact",true);
-		
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+
 		return mv;
-		
+
 	}
-	
-	@RequestMapping(value="/show/all/products")
-	public ModelAndView showAllProducts()
-	{
-		ModelAndView mv=new ModelAndView("page");
-		
-		mv.addObject("title","All Products");
-		//passing the list of categories
-		
+
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+
+		return mv;
+
+	}
+
+	@RequestMapping(value = "/show/all/products")
+	public ModelAndView showAllProducts() {
+		ModelAndView mv = new ModelAndView("page");
+
+		mv.addObject("title", "All Products");
+		// passing the list of categories
+
 		mv.addObject("categories", categoryDAO.list());
-		
-		mv.addObject("userClickAllProducts",true);
-		
+
+		mv.addObject("userClickAllProducts", true);
+
 		return mv;
-		
+
 	}
-	
-	@RequestMapping(value="/show/category/{id}/products")
-	public ModelAndView showCategoryProducts(@PathVariable("id") int id)
-	{
-		ModelAndView mv=new ModelAndView("page");
-		
-		//categoryDao to fetch a single category
+
+	@RequestMapping(value = "/show/category/{id}/products")
+	public ModelAndView showCategoryProducts(@PathVariable("id") int id) {
+		ModelAndView mv = new ModelAndView("page");
+
+		// categoryDao to fetch a single category
 		Category category = null;
-		
+
 		category = categoryDAO.get(id);
-		
-		
-		mv.addObject("title",category.getName());
-		//passing the list of categories
-		
+
+		mv.addObject("title", category.getName());
+		// passing the list of categories
+
 		mv.addObject("categories", categoryDAO.list());
-		
+
 		mv.addObject("category", category);
-		
-		mv.addObject("userClickCategoryProducts",true);
-		
+
+		mv.addObject("userClickCategoryProducts", true);
+
 		return mv;
-		
+
 	}
-	
 
 	@RequestMapping(value = "/show/{id}/product")
-	public ModelAndView showSingleProducts(@PathVariable int id) throws ProductNotFoundException{
-		
-		ModelAndView mv= new ModelAndView("page");
-		
+	public ModelAndView showSingleProducts(@PathVariable int id) throws ProductNotFoundException {
+
+		ModelAndView mv = new ModelAndView("page");
+
 		Product product = productDAO.get(id);
-		if(product == null) throw new ProductNotFoundException();
-		
+		if (product == null)
+			throw new ProductNotFoundException();
+
 		product.setViews(product.getViews() + 1);
 		productDAO.update(product);
-		
-		mv.addObject("title",product.getName());
-		mv.addObject("product",product);
-		
+
+		mv.addObject("title", product.getName());
+		mv.addObject("product", product);
+
 		mv.addObject("userClickShowProduct", true);
-		
+
 		return mv;
 	}
-	
-	/* having similar mapping to our flow id*/
-	@RequestMapping(value="/register")
-	public ModelAndView register()
-	{
-		ModelAndView mv=new ModelAndView("page");
-		
-		mv.addObject("title","About Us");
+
+	/* having similar mapping to our flow id */
+	@RequestMapping(value = "/register")
+	public ModelAndView register() {
+		ModelAndView mv = new ModelAndView("page");
+
+		mv.addObject("title", "About Us");
 		return mv;
-		
+
 	}
-
-
 
 	
 }
---
+++ shopping/src/main/webapp/WEB-INF/views/flows/shared/flows-footer.jsp
@@ -0,0 +1,25 @@
+</div>
+		<!-- Footer -->
+		<%@include file="../../shared/footer.jsp"%>
+
+		<!-- Bootstrap core JavaScript -->
+		<script src="${js}/jquery.js"></script>
+
+		<script src="${js}/jquery.validate.js"></script>
+
+		<script src="${js}/popper.js"></script>
+
+		<script src="${js}/bootstrap.min.js"></script>
+
+		<script src="${js}/dataTables.bootstrap.js"></script>
+
+		<script src="${js}/jquery.dataTables.js"></script>
+
+		<script src="${js}/bootbox.min.js"></script>
+
+		<script src="${js}/myapp.js"></script>
+
+	</div>
+</body>
+
+</html>
